62f9d8026c6349f6772a7106bdafef14ea8a6d7c
John Unland <unlandj2012@gmail.com>
2026-07-13 10:10:17 -0500

    Refactor run-dataplaneapi.sh for better readability
    Refactor run-dataplaneapi.sh to use arrays for configuration and lifecycle flags, improving readability and maintainability.

--- haproxy/scripts/run-dataplaneapi.sh
+++ haproxy/scripts/run-dataplaneapi.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2026 John Unland
 
-/usr/local/sbin/dataplaneapi --host 0.0.0.0 --port 5555 --haproxy-bin /usr/local/sbin/haproxy -f dataplaneapi.yml -c haproxy.cfg --reload-cmd "kill -SIGUSR2 1" --reload-delay 5 --restart-cmd "kill -SIGUSR2 1"
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Define configuration flags
+DATAPLANE_API_CONF_FLAGS=(
+    --host "0.0.0.0"
+    --port "5555"
+    --socket-path "/var/run/data-plane.sock"
+    --haproxy-bin "/usr/local/sbin/haproxy"
+    -f "dataplaneapi.yml"
+    -c "haproxy.cfg"
+)
+
+# Define lifecycle flags using an array to safely handle internal quotes
+DATAPLANE_API_LIFECYCLE_FLAGS=(
+    --reload-cmd "kill -SIGUSR2 1"
+    --reload-delay 5
+    --restart-cmd "kill -SIGUSR2 1"
+)
+
+# Execute dataplaneapi using array expansion
+exec /usr/local/sbin/dataplaneapi "${DATAPLANE_API_CONF_FLAGS[@]}" "${DATAPLANE_API_LIFECYCLE_FLAGS[@]}"
